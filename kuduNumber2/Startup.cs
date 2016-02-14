e2570c22670aa857a16842d41ee6534b1ea8c9a9
mujahidhafeez <mujahidhafeez79@gmail.com>
2016-02-14 23:59:23 +0500

    done

---
+++ kuduNumber2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kuduNumber2.Startup))]
+namespace kuduNumber2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
